3cb527b846edc6540f653f5eb16239e8d133db41
saaim <125155759+saaim12@users.noreply.github.com>
2025-03-26 10:08:03 +0500

    only for dockerization

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -1,6 +1,5 @@
 import mongoose, { Schema } from "mongoose";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
 const userSchema = new Schema(
   {
     username: {
@@ -31,10 +30,10 @@ const userSchema = new Schema(
 );
 userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) return next();
-    this.password = await bcrypt.hash(this.password, 8); 
+   // this.password = await bcrypt.hash(this.password, 8); 
 });
 userSchema.methods.isPasswordCorrect=async function (password) {
-  return await bcrypt.compare(password,this.password);
+ // return await bcrypt.compare(password,this.password);
 }
 userSchema.methods.generateAccessToken=function(){
    return jwt.sign({
